5d296a466a6b182111b468ebda798fc842d6a7f5
weborastudio1 <weborastudiozz@gmail.com>
2026-06-14 10:41:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -296,3 +296,17 @@ setInterval(() => {
   }, 700);
 
 }, 4000);
+
+
+document.querySelectorAll('.btn, .service-card').forEach(el => {
+  el.addEventListener('mousemove', e => {
+    const rect = el.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+    el.style.transform = `translate(${x * 0.08}px, ${y * 0.08}px)`;
+  });
+
+  el.addEventListener('mouseleave', () => {
+    el.style.transform = 'translate(0,0)';
+  });
+});
