6b548eb6aa715199defa0806f73dfebc5973faed
weborastudio1 <weborastudiozz@gmail.com>
2026-06-19 17:45:43 +0530

    Create data/products-data.js

---
+++ data/products-data.js
@@ -0,0 +1,32 @@
+<script>
+/* ===============================
+   PRODUCT CONTROL ROOM
+   =============================== */
+
+const PRODUCTS = {
+  silk_rose_scrunchie: {
+    id: "silk-rose-scrunchie",
+
+    brand: "Dreamy Scrunch",
+    name: "Silk Rose Scrunchie",
+
+    price: "199",
+    originalPrice: "299",
+    discount: "33% OFF",
+
+    description:
+      "Luxurious handmade silk scrunchie designed for comfort, shine & elegance.",
+
+    color: "Rose Pink",
+    material: "Premium Silk",
+
+    instagramUsername: "dreamyscrunch",
+
+    images: [
+      "../images/silk-rose-scrunchie/1.jpg",
+      "../images/silk-rose-scrunchie/2.jpg",
+      "../images/silk-rose-scrunchie/3.jpg"
+    ]
+  }
+};
+</script>
