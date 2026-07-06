9db1a651b714aac7da88e050587cfac672c9eeea
weborastudio1 <weborastudiozz@gmail.com>
2026-07-06 09:49:14 +0530

    Create products-data.js

---
+++ products/products-data.js
@@ -0,0 +1,84 @@
+// products/products-data.js
+
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "Pink Velvet Scrunchie",
+    price: 199,
+    image: "images/p1.jpg",
+    desc: "Soft velvet scrunchie for daily wear",
+    category: "scrunchie"
+  },
+  {
+    id: 2,
+    name: "Floral Hair Band",
+    price: 149,
+    image: "images/p2.jpg",
+    desc: "Cute floral band for girls",
+    category: "band"
+  },
+  {
+    id: 3,
+    name: "Silk Scrunchie",
+    price: 249,
+    image: "images/p3.jpg",
+    desc: "Premium silk smooth scrunchie",
+    category: "scrunchie"
+  },
+  {
+    id: 4,
+    name: "Black Bow Clip",
+    price: 99,
+    image: "images/p4.jpg",
+    desc: "Elegant black bow hair clip",
+    category: "clip"
+  },
+  {
+    id: 5,
+    name: "Pastel Combo Pack",
+    price: 399,
+    image: "images/p5.jpg",
+    desc: "5 pastel scrunchies combo",
+    category: "combo"
+  },
+  {
+    id: 6,
+    name: "Rose Gold Clip",
+    price: 129,
+    image: "images/p6.jpg",
+    desc: "Stylish rose gold clip",
+    category: "clip"
+  },
+  {
+    id: 7,
+    name: "Kids Hair Bands",
+    price: 179,
+    image: "images/p7.jpg",
+    desc: "Cute colorful bands for kids",
+    category: "band"
+  },
+  {
+    id: 8,
+    name: "Wedding Scrunchie",
+    price: 299,
+    image: "images/p8.jpg",
+    desc: "Luxury scrunchie for weddings",
+    category: "scrunchie"
+  },
+  {
+    id: 9,
+    name: "Mini Clips Set",
+    price: 159,
+    image: "images/p9.jpg",
+    desc: "Set of 6 mini clips",
+    category: "clip"
+  },
+  {
+    id: 10,
+    name: "Daily Use Combo",
+    price: 349,
+    image: "images/p10.jpg",
+    desc: "Best combo for daily styling",
+    category: "combo"
+  }
+];
