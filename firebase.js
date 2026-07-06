3722d3171111ab6b6b8270f969844a88bb723f4a
weborastudio1 <weborastudiozz@gmail.com>
2026-07-06 10:49:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,14 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔥 Firebase Config (Values Added)
 const firebaseConfig = {
   apiKey: "AIzaSyDQ8x3KFmgEsfH1AQxsy0ljiBcIeX7tug8",
   authDomain: "dreamy-scrunch.firebaseapp.com",
   projectId: "dreamy-scrunch",
   storageBucket: "dreamy-scrunch.firebasestorage.app",
   messagingSenderId: "927358366614",
-  appId: "1:927358366614:web:56a8b014baa05c1c9423b7"
+  appId: "1:927358366614:web:c1ce8b54b6a2e3379423b7"
 };
 
-// 🚀 Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// 📦 Export Firestore DB
 export const db = getFirestore(app);
